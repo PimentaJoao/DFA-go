674652fd77936c80eb828d2b8e5d5e0b0d784dff
PimentaJoao <jppimentas@hotmail.com>
2021-06-20 18:40:15 -0300

    add support for multiple files at the same time

--- main.go
+++ main.go
@@ -2,23 +2,38 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 
 	"github.com/PimentaJoao/DFA-go/AFD"
 )
 
 func main() {
 
-	afd := AFD.New("./examples/example1.txt")
+	// Lê o diretório "/exemples".
+	files, _ := ioutil.ReadDir("./examples")
 
-	wordTest := AFD.Process(afd)
+	// Para cada arquivo encontrado, cria seu AFD e analisa suas palavras testes.
+	for _, file := range files {
+		fmt.Print("\nARQUIVO: ", file.Name(), "\n\n\n")
 
-	for _, test := range wordTest {
-		fmt.Print("Palavra: ", test.Word, "\n")
-		fmt.Println("Estados percorridos: ")
-		for _, w := range test.States {
-			fmt.Print(" -> ", w)
+		// Constrói o caminho para o arquivo encontrado.
+		path := "./examples/" + file.Name()
+
+		// Constrói o AFD.
+		afd := AFD.New(path)
+
+		// Testa os exemplos que vieram com o AFD.
+		wordTest := AFD.Process(afd)
+
+		// Imprime resultado dos testes.
+		for _, test := range wordTest {
+			fmt.Print("Palavra: ", test.Word, "\n")
+			fmt.Println("Estados percorridos: ")
+			for _, w := range test.States {
+				fmt.Print(" -> ", w)
+			}
+			fmt.Print("\nPalavra foi aceita?: ", test.AcceptedStatus, "\n\n")
 		}
-		fmt.Print("\nPalavra foi aceita?: ", test.AcceptedStatus, "\n\n")
 	}
 
 }
